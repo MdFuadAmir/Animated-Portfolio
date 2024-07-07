8b14a3fa3525e66efc8f44bf313aa8729475ec77
Md Fuad Amir <mdfuadamir@gmail.com>
2024-07-08 00:39:03 +0600

    added Client section

---
+++ src/Components/Client/Client.jsx
@@ -0,0 +1,29 @@
+
+
+const Client = () => {
+    return (
+        <div className="py-24 px-4 md:px-24">
+            <div className=" grid grid-cols-2 md:grid-cols-4 gap-4">
+                <div className=" space-y-2 text-center">
+                    <h1 className="text-4xl md:text-6xl text-white font-bold">2</h1>
+                    <p className="text-white">Years of Experience</p>
+                </div>
+                <div className=" space-y-2 text-center">
+                    <h1 className="text-4xl md:text-6xl text-white font-bold">38+</h1>
+                    <p className="text-white">Project Complete</p>
+                </div>
+                <div className=" space-y-2 text-center">
+                    <h1 className="text-4xl md:text-6xl text-white font-bold">0</h1>
+                    <p className="text-white">Happy Client</p>
+                </div>
+                <div className=" space-y-2 text-center">
+                    <h1 className="text-4xl md:text-6xl text-white font-bold">4</h1>
+                    <p className="text-white">Years of Sturdy</p>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Client;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Client from "../Client/Client";
 import Hero from "../Hero/Hero";
 import "../Navber/navber.css"
 
@@ -5,6 +6,7 @@ const Home = () => {
     return (
         <div className="">
             <Hero/>
+            <Client/>
         </div>
     );
 };
